57e164a680d7d1042628d3f2148052e00ea04479
Adam Gregorczuk <adam.gregorczuk@gmail.com>
2013-08-13 09:58:50 +0200

    add teams api correct - team model

--- src/intranet3/models/team.py
+++ src/intranet3/models/team.py
@@ -2,9 +2,8 @@
 from sqlalchemy import Column, ForeignKey, orm
 from sqlalchemy.types import Integer, String
 from sqlalchemy.schema import UniqueConstraint
-from sqlalchemy.sql.expression import exists
 
-from intranet3.models import Base, User, DBSession
+from intranet3.models import Base, User
 
 class Team(Base):
     __tablename__ = 'teams'
@@ -15,20 +14,7 @@ class Team(Base):
     
     def to_dict(self):
         return {'id': self.id, 'name': self.name}
-        
-    def validate(self):
-        error = {'errors': False, 'name': []}
-        if len(self.name)>255:
-            error['errors'] = True
-            error['name'].append('for_long')
-        if DBSession.query(exists().where(Team.name==self.name)).scalar():
-            error['errors'] = True
-            error['name'].append('exists')
-        if error['errors'] == True:
-            return error
-        else:
-            return True
-        
+    
     
 class TeamMember(Base):
     __tablename__ = 'team_members'
